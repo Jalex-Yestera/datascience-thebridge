8a4251e34de5fb32aa59abcaa63d382a6ecd0473
Jalex-Yestera <yestera@gmail.com>
2020-07-12 20:15:14 +0200

    Ejercicios W4 j_son enviados no subidos

--- week4/day1/delivery/proyectos_modulo.py
+++ week4/day1/delivery/proyectos_modulo.py
@@ -1,14 +1,46 @@
-# This file represents your module.
 
-MODULO DE FUNCIONES EN ESTADO BRAINSTORMING
 
-def cortarte_las_venas ():
-    print ('¿Has Probado a reiniciarte TU?')
+# MODULO DE FUNCIONES EN ESTADO BRAINSTORMING 
+'''            BRAINSTORMING              '''
+###            braistorming               
+
+# Generar archivo con la lista de la compra
+def lista_compra ():
+    print ("Jalou!:","\n", "Para añadir artículos a la lista escribe '1' ", "\n", "Para ver la lista escribe '2'")
+    inicio = input("Elige una opción (1 o 2)") 
+    if inicio == 1:
+        anadir()
+    else:
+        print("La lista actualizada es:" , lista_imprimir)
+    # Definicion de funciones basicas que son anadir e imprimir:
+    def anadir ():
+        nuevo = []
+        art = input ("Añade tu articulo a la lista:")
+        cant = int(input ("Escribe la cantidad"))
+        nuevo.append (art)
+        # nuevo = nuevo + ['cant','art']
+        print ("se añade", cant, art , "al pedido")
+        print (nuevo)
+        if art !=0 :
+            anadir ()
+        else:
+            guardar_archivo()
+
+    with open('lista_compra.txt', 'a') as outfile:
+        print(type(outfile))
+        lista_imprimir = lista_imprimir + anadir
+
+
 
 
 
 def aladino ():
     #Esta función realiza tres tareas y se autodestruye
+
+
+
+def capitan_calzoncillos():
+    # Esta funcion le dices cuantos dias te vas de viaje y devuelve una tabla de lunes a domingo con el total de calzoncillos.
 
 
 
@@ -20,4 +52,6 @@ def deus_est_machina ():
 def Tio_Gilito ():
     # Función para llenar de pasta mis bolsillos
 
-    def Daigual_lo_que_comas ():
+
+
+def Daigual_lo_que_comas ():
