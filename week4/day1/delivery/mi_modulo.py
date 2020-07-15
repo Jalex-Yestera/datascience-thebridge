8d8ef06dbe2c88ae57f0f3e8ad3c213347342d25
Jalex-Yestera <yestera@gmail.com>
2020-07-15 21:18:53 +0200

    W5d3

--- week4/day1/delivery/mi_modulo.py
+++ week4/day1/delivery/mi_modulo.py
@@ -62,6 +62,21 @@ def tsv_df (x):
 
 
 
+# Funcion descarga archivo al disco
+def pabajo():
+    import requests
+    download_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv"
+    target_csv_path = "nba_all_elo.csv"
+    response = requests.get(download_url)
+    response.raise_for_status() # Check that the request was successful
+    with open(target_csv_path, "wb") as f:
+        f.write(response.content)
+    print("Download ready.")
+
+
+
+
+
 # Funcion entrega W5
 def _f_protected(x):
     l1 = []
@@ -72,8 +87,15 @@ fv
 
 
 def prepost(*arg, **kwargs):
-
-    
-    
-    
+    import pandas as pd
+    import requests
+    if 'url' in kwargs:
+        df = pd.read_csv('url')
+        _f_protected()
+        df = pd.DataFrame(df)
+        df.hist()
 @prepost(url=key_url) 
+
+
+
+
