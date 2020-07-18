7afa30033bb3c4731c38cda7b1732bd15988cdce
Jalex-Yestera <yestera@gmail.com>
2020-07-18 21:23:47 +0200

    Titanic 52%

--- week5/day2/exercises/python/Testfile.py
+++ week5/day2/exercises/python/Testfile.py
@@ -1,15 +1,12 @@
 # To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
+
 import pandas as pd
 import numpy as np
 import requests
 from functools import wraps
 
 
-# %%
 def prepost (*args,**kwargs):
-    print ("Gráfico con medallas de poco interés en España")
     def inner (function):
         @wraps(function)
         def wrapper(*a, **k):
@@ -21,15 +18,10 @@ def prepost (*args,**kwargs):
             return retval
         return wrapper
     return inner
+
+
 @prepost(url="http://winterolympicsmedals.com/medals.csv")
 def _f_protected ():
     l1 = [j for j in range(16)]
     fv= list(filter(lambda x : x > 5,l1)) 
     return fv
-_f_protected()
-
-
-# %%
-
-# %%
-
